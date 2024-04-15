aa7ac2ea865d1da480dae573da8bfeeb9a4bc237
Maxim Kucherov <maxima-net@bk.ru>
2024-04-15 15:43:09 +0300

    Add global error pages

---
+++ src/app/global-error.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from 'next/link';
+import { NoData } from './ui/common';
+
+interface GlobalErrorProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+const GlobalError = ({ error, reset }: GlobalErrorProps) => {
+  return (
+    <html>
+      <body className="flex grow-1 min-h-screen">
+        <div className="container flex items-center justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <NoData text="500 | Something went wrong" />
+            <Link href="/">Home</Link>
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+};
+
+export default GlobalError;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import { NoData } from './ui/common';
+
+const NotFound = () => {
+  return <NoData text="404 | Page not found" />
+};
+
+export default NotFound;
